39837eb3bc31b2b1e4cda78a0ed6ce106f988838
Petersamy18 <62220779+Petersamy18@users.noreply.github.com>
2022-08-06 18:31:59 +0200

    Creating database files

--- app.py
+++ app.py
@@ -1,7 +1,19 @@
-from flask import Flask
+from flask import Flask, request, redirect, url_for
+from flask import render_template
+from flask import request
+import sqlite3
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
     return "<p>Hello, World!</p>"
+
+@app.route("/Login/", methods = ['GET', 'POST'])
+def login():
+    if request.methods == 'POST':
+
+        return
+    
+    return render_template()
+
---
+++ initDB.py
@@ -0,0 +1,30 @@
+from db import get_db, close_db
+import sqlite3
+
+def create_tables():
+    try:
+
+        connection = get_db()
+        connection.execute("""
+                            CREATE TABLE User (
+                            User_id INTEGER PRIMARY KEY AUTOINCREMENT,
+                            Email   TEXT UNIQUE NOT NULL,
+                            Username TEXT UNIQUE NOT NULL,
+                            Password TEXT NOT NULL);
+                            """)
+
+        connection.execute("""
+                            CREATE TABLE Task (
+                            Task_id INTEGER PRIMARY KEY AUTOINCREMENT,
+                            Title   TEXT UNIQUE NOT NULL,
+                            Describtion TEXT UNIQUE NOT NULL,
+                            Status TEXT NOT NULL,
+                            User_id INTEGER);
+                            """)
+        connection.commit()
+        close_db(connection)
+                    
+    except Exception as err:
+        print("Error")
+
+create_tables()
